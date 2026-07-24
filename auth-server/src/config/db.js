1a2839b6a0ef8a12fc89eba99d7ef71a656d7468
Aryan Tomar <aryantomar793@gmail.com>
2026-07-24 22:16:39 +0530

    .env fix

--- auth-server/src/config/db.js
+++ auth-server/src/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://aryantomar32155:ARYan32155@cluster0.pvw5baf.mongodb.net/");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
